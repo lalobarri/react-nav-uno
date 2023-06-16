de5d1e9587ec3de7b94d16cfa1ade8d39783f20a
Eduardo Barrientos <eduardoBarri@gmail.com>
2023-06-16 15:19:47 -0600

    Segundo avance

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,29 @@
+import '../css/estilos.css';
+
 const About = () => {
     return(
         <div>
             <h1>About View</h1>
             <p>Esta es la vista About</p>
             <p>Datos del desarrollador</p>
+
+            <div>
+                <form class="formulario" method="get">
+                    <h1>Login</h1>
+                    <div class="contenedor">
+                        <div class="input-contenedor">
+                            <input type="text" placeholder="Correo"></input>
+                        </div>
+                        <div class="input-contenedor">
+                            <input type="password" placeholder="Contraeña"></input>
+                        </div>
+                        <input type="submit" value="Login" class="button" />
+                        <p>Al registrarte, aceptas nuestras Condiciones de uso y 
+                            Política de privacidad.</p>
+                    </div>
+    
+                </form>
+            </div>
 
         </div>
     );
--- src/components/home.js
+++ src/components/home.js
@@ -5,6 +5,19 @@ const Home = () => {
             <p>Esta es la vista Home</p>
             <p>Datos de Introducción a la Aplicación</p>
 
+            <div class="mb-3">
+                <label for="mail" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="mail" 
+                placeholder="name@example.com"></input>
+
+            </div>
+
+            <div class="mb-3">
+                <label for="text-area" class="form-label">Text Area Example</label>
+                <textarea class="form-control" id="text-area" rows="3"></textarea>
+            </div>
+
+
         </div>
     );
 }
